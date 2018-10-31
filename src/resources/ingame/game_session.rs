706ece6d15a4698ad0cdfd00f321afea4fa1be60
Telzhaak <da.ahrens@outlook.de>
2018-10-31 13:20:59 +0100

    Savegame improvements/refactor

--- src/resources/ingame/game_session.rs
+++ src/resources/ingame/game_session.rs
@@ -1,5 +1,19 @@
-use super::planet::Planet;
+use std::{
+    fs,
+};
+
+use ron;
+use serde::Serializer;
+
+use amethyst::ecs::prelude::{Read};
+
 use resources::RenderConfig;
+
+use super::{
+    planet::Planet,
+    SavegamePaths,
+};
+
 /// Data specific to the current game,
 /// gets loaded or created when the User starts a new game.
 #[derive(Debug, Serialize, Deserialize)]
@@ -25,5 +39,52 @@ impl GameSessionData {
             game_name: name,
             planet: Planet::new(planet_dim, chunk_dim, render_config),
         }
+    }
+
+    /// TODO: Error handling
+    pub fn save(&self, paths: &Read<'_, SavegamePaths>) {
+        #[cfg(feature = "debug")]
+        debug!("| Starting to serialize savegame.");
+
+        #[cfg(feature = "debug")]
+        debug!("| Serializing game data.");
+
+        let planet = &self.planet;
+
+        let mut ser_planet = ron::ser::Serializer::new(Some(Default::default()), true);
+        {
+            use serde::ser::SerializeSeq;
+            if let Ok(mut serseq) = ser_planet.serialize_seq(None) {
+                if let Err(e) = serseq.serialize_element(&self) {
+                    error!("| Error serializing element planet: {:?}", e);
+                }
+                if let Err(e) = serseq.end() {
+                    error!("| Error ending serialize for planet: {:?}", e);
+                }
+            } else {
+                error!("| Error starting serialize for planet.");
+            }
+        }
+        // TODO: Write to file `{$savegame_name}/planet.ron`
+        if let Err(e) = fs::write(
+            paths.planet_file_path.clone(),
+            ser_planet.into_output_string(),
+        ) {
+            error!(
+                "| Writing savegame planet at '{:?}' threw error: {:?}",
+                paths.planet_file_path.clone(),
+                e
+            );
+        }
+
+        #[cfg(feature = "debug")]
+        debug!("| serializing chunks.");
+
+        for (&chunk_index, _) in planet.iter_chunks() {
+            planet.save_chunk(chunk_index, paths.chunk_dir_path.clone());
+        }
+
+        #[cfg(feature = "debug")]
+        debug!("| Finished serializing savegame.");
     }
 }
--- src/systems/serialization/ser.rs
+++ src/systems/serialization/ser.rs
@@ -42,55 +42,17 @@ impl<'a> System<'a> for SerSavegameSystem {
     );
 
     fn run(&mut self, (session_data, render_config, paths): Self::SystemData) {
-        if let (Some(session_data), Some(render_config), Some(paths)) =
+        #[cfg(feature = "debug")]
+        debug!("+------------");
+
+        if let (Some(session_data), Some(_render_config), Some(paths)) =
             (session_data, render_config, paths)
         {
-            #[cfg(feature = "debug")]
-            debug!("Starting to serialize savegame.");
-
-            #[cfg(feature = "debug")]
-            debug!("Serializing game data.");
-
-            let session_data = session_data.deref();
-            let planet = &session_data.planet;
-
-            let mut ser_planet = ron::ser::Serializer::new(Some(Default::default()), true);
-            {
-                use serde::ser::SerializeSeq;
-                if let Ok(mut serseq) = ser_planet.serialize_seq(None) {
-                    if let Err(e) = serseq.serialize_element(session_data) {
-                        error!("Error serializing element planet: {:?}", e);
-                    }
-                    if let Err(e) = serseq.end() {
-                        error!("Error ending serialize for planet: {:?}", e);
-                    }
-                } else {
-                    error!("Error starting serialize for planet.");
-                }
-            }
-            // TODO: Write to file `{$savegame_name}/planet.ron`
-            if let Err(e) = fs::write(
-                paths.planet_file_path.clone(),
-                ser_planet.into_output_string(),
-            ) {
-                error!(
-                    "Writing savegame planet at '{:?}' threw error: {:?}",
-                    paths.planet_file_path.clone(),
-                    e
-                );
-            }
-
-            #[cfg(feature = "debug")]
-            debug!("serializing planet.");
-
-            for (&chunk_index, _) in planet.iter_chunks() {
-                planet.save_chunk(chunk_index, paths.chunk_dir_path.clone());
-            }
-
-            #[cfg(feature = "debug")]
-            debug!("Finished serializing savegame.");
+            session_data.save(&paths);
         } else {
-            error!("Resources not found.")
+            error!(" Resources not found.")
         }
+        #[cfg(feature = "debug")]
+        debug!("+------------");
     }
 }
